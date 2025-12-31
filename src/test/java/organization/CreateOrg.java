82fd1f4be33226e395703f15079f6eb862740f31
divyacl123 <divyacl020@gmail.com>
2025-12-31 17:05:17 +0530

    framework is ready

---
+++ src/test/java/organization/CreateOrg.java
@@ -0,0 +1,12 @@
+package organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg {
+	
+	@Test
+	public void createOrg() {
+		System.out.println("---create organization---");
+	}
+
+}
